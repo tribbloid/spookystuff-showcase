859d47681e2ce6e889f9f00e3b074bab8832d061
Peng Cheng <peng.cheng@acyclic.ai>
2023-06-27 14:31:47 -0400

    add the missing spark dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,7 @@ val vs = versions()
 dependencies {
 
     api(project(":parent:web"))
+
+    implementation("org.apache.spark:spark-sql_${vs.scala.binaryV}:${vs.sparkV}")
+    implementation("org.apache.spark:spark-mllib_${vs.scala.binaryV}:${vs.sparkV}")
 }
