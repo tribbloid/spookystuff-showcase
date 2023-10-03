ecf87982383df8270795ccf47045f98f1ca5b4f0
peng <peng.cheng@acyclic.ai>
2023-10-03 15:36:31 -0400

    INCOMPLETE

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,85 @@
 
 val vs = versions()
 
-dependencies {
+plugins {
 
-    api(project(":parent:web"))
+    scala
+    id("io.github.cosmicsilence.scalafix") version "0.1.14"
+}
 
-    implementation("org.apache.spark:spark-sql_${vs.scala.binaryV}:${vs.sparkV}")
-    implementation("org.apache.spark:spark-mllib_${vs.scala.binaryV}:${vs.sparkV}")
+subprojects {
+    apply(plugin = "scala")
+
+    tasks {
+        withType<ScalaCompile> {
+
+            scalaCompileOptions.apply {
+                loggingLevel = "verbose"
+
+                val compilerOptions =
+
+                    mutableListOf(
+
+                        "-encoding", "UTF-8",
+                        "-unchecked", "-deprecation", "-feature",
+
+                        "-g:vars",
+
+                        "-Xlint",
+                    )
+
+                additionalParameters = compilerOptions
+
+                forkOptions.apply {
+
+                    memoryInitialSize = "1g"
+                    memoryMaximumSize = "4g"
+
+                    // this may be over the top but the test code in macro & core frequently run implicit search on church encoded Nat type
+                    jvmArgs = listOf(
+                        "-Xss256m"
+                    )
+                }
+            }
+        }
+
+        test {
+
+            minHeapSize = "1024m"
+            maxHeapSize = "4096m"
+
+            testLogging {
+
+                showExceptions = true
+                exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+
+                showCauses = true
+                showStackTraces = true
+
+                // stdout is used for occasional manual verification
+                showStandardStreams = true
+            }
+
+            useJUnitPlatform {
+                includeEngines("scalatest")
+                testLogging {
+                    events("passed", "skipped", "failed")
+                }
+
+                val p = this@subprojects
+
+                if (p.hasProperty("notLocal") ) {
+                    excludeTags("com.tribbloids.spookystuff.testutils.LocalOnly")
+                }
+            }
+        }
+    }
+
+    dependencies {
+
+        api(project(":parent:web"))
+
+        implementation("org.apache.spark:spark-sql_${vs.scala.binaryV}:${vs.sparkV}")
+        implementation("org.apache.spark:spark-mllib_${vs.scala.binaryV}:${vs.sparkV}")
+    }
 }
---
+++ showcase/src/main/scala/com/tribbloids/spookystuff/showcase/SpookyEnv.scala
@@ -0,0 +1,51 @@
+package com.tribbloids.spookystuff.showcase
+
+import com.tribbloids.spookystuff.SpookyContext
+import com.tribbloids.spookystuff.utils.CommonConst
+import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by peng on 22/06/14. allowing execution as a main object and tested as a test class keep each test as small
+  * as possible, by using downsampling & very few iterations
+  */
+trait SpookyEnv {
+
+  val appName: String = this.getClass.getSimpleName.replace("$", "")
+
+  val spark: SparkSession = {
+
+    val conf: SparkConf = new SparkConf().setAppName(appName)
+
+    val master = conf
+      .getOption("spark.master") // if submitted directly, this will be the value
+      .orElse(Option(System.getenv("MASTER")))
+      .getOrElse(s"local[${Runtime.getRuntime.availableProcessors()},10]")
+
+    conf.setMaster(master)
+
+    SparkSession.builder().config(conf).appName(appName).getOrCreate()
+  }
+  lazy val sc: SparkContext = spark.sparkContext
+  lazy val sql: SQLContext = spark.sqlContext
+
+  def getSpooky(args: Array[String]): SpookyContext = {
+    // args deliberately ignored for being hard to integrate
+
+    val spooky = new SpookyContext(sql)
+
+    val dirs = spooky.dirConf
+
+//    if (dirs.root == null) {
+//      dirs.root = s"file://${CommonConst.USER_DIR}/temp/spooky-local/$appName/"
+//    }
+//
+//    if (dirs.cache == null) {
+//      dirs.cache = s"file://${CommonConst.USER_DIR}/temp/spooky-local/cache/"
+//    }
+
+//    spooky.spookyConf.shareMetrics = true
+
+    spooky
+  }
+}
