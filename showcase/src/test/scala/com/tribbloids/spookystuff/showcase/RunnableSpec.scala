75fde51cc6469665ba0cf47f6177e4803c4673d7
peng <peng.cheng@acyclic.ai>
2023-10-12 15:21:05 -0400

    RunnableSpec is now abstract

--- showcase/src/test/scala/com/tribbloids/spookystuff/showcase/RunnableSpec.scala
+++ showcase/src/test/scala/com/tribbloids/spookystuff/showcase/RunnableSpec.scala
@@ -3,7 +3,7 @@ package com.tribbloids.spookystuff.showcase
 import com.tribbloids.spookystuff.notebook.CIBCExtractData
 import org.scalatest.funspec.AnyFunSpec
 
-class RunnableTest extends AnyFunSpec {
+abstract class RunnableSpec extends AnyFunSpec {
 
   val runnables = Seq(
     CIBCExtractData
