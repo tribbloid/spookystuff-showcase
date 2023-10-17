91df2d2dfd6f834834305255dd4bbcb66cf1d3eb
peng <peng.cheng@acyclic.ai>
2023-10-17 10:45:17 -0400

    remove a comment

--- src/main/scala/com/tribbloids/spookystuff/notebook/CIBCExtractIncome.scala
+++ src/main/scala/com/tribbloids/spookystuff/notebook/CIBCExtractIncome.scala
@@ -10,63 +10,6 @@ object CIBCExtractIncome extends SpookyRunnable {
     "file://~/Synced/Dropbox/__CORPORATE/BalanceSheet/DepositAccount_10012023.pdf"
   }
 
-//  override def doMain(): Unit = {
-//
-//    val fetched = spooky
-//      .fetch(
-//        Wget(imageURL)
-//      )
-//      .select(Lit("Wikipedia") ~ 'name)
-//      .persist()
-//    //    fetched.count()
-//
-//    val RDD = fetched
-//      .savePages_!(x"file://${CommonConst.USER_DIR}/temp/spooky-integration/save/${'name}", overwrite = true)
-//      .select(S.saved ~ 'saved_path)
-//
-//    val savedPageRows = RDD.unsquashedRDD.collect()
-//
-//    val finishTime = System.currentTimeMillis()
-//    assert(savedPageRows.length === 1)
-//    assert(savedPageRows(0).docs.length === 1)
-//    val pageTime = savedPageRows(0).docs.head.timeMillis
-//    assert(pageTime < finishTime)
-//    assert(pageTime > finishTime - 60000) // long enough even after the second time it is retrieved from s3 cache
-//
-//    val content = savedPageRows(0).docs.head.raw
-//
-//    assert(
-//      savedPageRows(0).dataRow.get('saved_path).get.asInstanceOf[Iterable[Any]].toSeq contains
-//        s"file:${CommonConst.USER_DIR}/temp/spooky-integration/save/Wikipedia.png"
-//    )
-//
-//    val loadedContent =
-//      DocUtils.load(s"file://${CommonConst.USER_DIR}/temp/spooky-integration/save/Wikipedia.png")(spooky)
-//
-//    assert(loadedContent === content)
-//
-//    Thread.sleep(10000) // this delay is necessary to circumvent eventual consistency of HDFS-based cache
-//
-//    val RDD2 = RDD
-//      .fetch(
-//        Wget(imageURL).as('b)
-//      )
-//
-//    val unionRDD = RDD.union(RDD2)
-//    val unionRows = unionRDD.unsquashedRDD.collect()
-//
-//    assert(unionRows.length === 2)
-//    assert(
-//      unionRows(0).docs.head.copy(timeMillis = 0, raw = null, saved = null) ===
-//        unionRows(1).docs.head.copy(timeMillis = 0, raw = null, saved = null)
-//    )
-//
-//    assert(unionRows(0).docs.head.timeMillis === unionRows(1).docs.head.timeMillis)
-//    assert(unionRows(0).docs.head.raw === unionRows(1).docs.head.raw)
-//    assert(unionRows(0).docs.head.raw === unionRows(1).docs.head.raw)
-//    assert(unionRows(1).docs.head.name === "b")
-//  }
-
   override def doMain(spooky: SpookyContext): Any = {
 
     spooky
