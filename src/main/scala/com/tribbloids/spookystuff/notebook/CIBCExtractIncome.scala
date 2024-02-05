1974aea930612ed76181b64257cf64d067f469ac
peng <peng.cheng@acyclic.ai>
2024-02-05 15:58:55 -0500

    minor simplification

--- src/main/scala/com/tribbloids/spookystuff/notebook/CIBCExtractIncome.scala
+++ src/main/scala/com/tribbloids/spookystuff/notebook/CIBCExtractIncome.scala
@@ -3,18 +3,16 @@ package com.tribbloids.spookystuff.notebook
 import ai.acyclic.prover.commons.spark.Envs
 import com.tribbloids.spookystuff.SpookyContext
 import com.tribbloids.spookystuff.actions._
+import com.tribbloids.spookystuff.dsl._
 import com.tribbloids.spookystuff.showcase.SpookyRunnable
-import com.tribbloids.spookystuff.utils.CommonUtils
-
-import java.io.File
 
 object CIBCExtractIncome extends SpookyRunnable {
 
   val file: String =
-    Envs.USER_HOME :\ "Synced/Dropbox/__CORPORATE/BalanceSheet/DepositAccount_10012023.pdf".replaceAllLiterally(
-      "/",
-      File.pathSeparator
-    )
+    Envs.USER_HOME :\ "Synced/Dropbox/__CORPORATE/BalanceSheet/DepositAccount_10012023.pdf"
+
+//  val file =
+//    Envs.USER_HOME :\ "Desktop" :\ "spark_survey.pdf"
 
   override def doMain(spooky: SpookyContext): Any = {
 
@@ -22,6 +20,25 @@ object CIBCExtractIncome extends SpookyRunnable {
       .fetch(
         Wget(file)
       )
-      .savePages_!(CommonUtils.\\\(Envs.USER_HOME, "Desktop", "yyy"), overwrite = true)
+      .extract(
+        S"head title" ~ 'title,
+        S.andMap(v => v.contentType.toString) ~ 'type,
+        S.andMap(v => v.mimeType) ~ 'mime
+      )
+      .savePages_!(
+        Envs.USER_HOME :\ "Desktop" :\ "__TEMP" :\ "xxx",
+        overwrite = true,
+        page = S
+      )
+      .savePages_!(
+        Envs.USER_HOME :\ "Desktop" :\ "__TEMP" :\ "yyy",
+        overwrite = true,
+        page = S
+          .andFlatMap { v =>
+            v.converted.docForAuditing
+          }
+      )
+      .toDF(sort = true)
+      .show(false)
   }
 }
